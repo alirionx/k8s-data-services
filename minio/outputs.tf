fb5e785fad7bd98c69356b71a284ad0ab8f81f24
alirionx <dquilitzsch@outlook.de>
2024-05-27 17:23:48 +0200

    scsc

--- minio/outputs.tf
+++ minio/outputs.tf
@@ -9,13 +9,15 @@ data "kubernetes_resources" "minio_service" {
 
 output "service_enpoint_info" {
   depends_on = [data.kubernetes_resources.minio_service]
-  value = {
-    internal_ip: data.kubernetes_resources.minio_service.objects[0].spec.clusterIP,
-    api_port: data.kubernetes_resources.minio_service.objects[0].spec.ports[0].port,
-    console_port: data.kubernetes_resources.minio_service.objects[0].spec.ports[1].port,
-    node_port_api: var.service_endpoint_type == "NodePort" ? data.kubernetes_resources.minio_service.objects[0].spec.ports[0].nodePort : "",
-    node_port_console: var.service_endpoint_type == "NodePort" ? data.kubernetes_resources.minio_service.objects[0].spec.ports[1].nodePort : "",
-    internal_dns: "minio-${var.service_id}.${var.service_namespace}",
-    external_ip: var.service_endpoint_type == "LoadBalancer" ? data.kubernetes_resources.minio_service.objects[0].status.loadBalancer.ingress[0].ip: ""
-  }
+  value = jsonencode( 
+    {
+      internal_ip: data.kubernetes_resources.minio_service.objects[0].spec.clusterIP,
+      api_port: data.kubernetes_resources.minio_service.objects[0].spec.ports[0].port,
+      console_port: data.kubernetes_resources.minio_service.objects[0].spec.ports[1].port,
+      node_port_api: var.service_endpoint_type == "NodePort" ? data.kubernetes_resources.minio_service.objects[0].spec.ports[0].nodePort : "",
+      node_port_console: var.service_endpoint_type == "NodePort" ? data.kubernetes_resources.minio_service.objects[0].spec.ports[1].nodePort : "",
+      internal_dns: "minio-${var.service_id}.${var.service_namespace}",
+      external_ip: var.service_endpoint_type == "LoadBalancer" ? data.kubernetes_resources.minio_service.objects[0].status.loadBalancer.ingress[0].ip: ""
+    }
+  )
 }
