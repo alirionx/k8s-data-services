8072fcd7ce633aabf65c3bc62cb7e3d648137ac1
alirionx <dquilitzsch@outlook.de>
2024-05-27 17:06:40 +0200

    noch nen test wegen den outputs

--- couchdb/outputs.tf
+++ couchdb/outputs.tf
@@ -9,11 +9,13 @@ data "kubernetes_resources" "couchdb_service" {
 
 output "service_enpoint_info" {
   depends_on = [data.kubernetes_resources.couchdb_service]
-  value = {
-    internal_ip: data.kubernetes_resources.couchdb_service.objects[0].spec.clusterIP,
-    port: data.kubernetes_resources.couchdb_service.objects[0].spec.ports[0].port,
-    node_port: var.service_endpoint_type == "NodePort" ? data.kubernetes_resources.couchdb_service.objects[0].spec.ports[0].nodePort : "",
-    internal_dns: "couchdb-${var.service_id}.${var.service_namespace}",
-    external_ip: var.service_endpoint_type == "LoadBalancer" ? data.kubernetes_resources.couchdb_service.objects[0].status.loadBalancer.ingress[0].ip: ""
-  }
+  value = jsonencode( 
+    {
+      internal_ip: data.kubernetes_resources.couchdb_service.objects[0].spec.clusterIP,
+      port: data.kubernetes_resources.couchdb_service.objects[0].spec.ports[0].port,
+      node_port: var.service_endpoint_type == "NodePort" ? data.kubernetes_resources.couchdb_service.objects[0].spec.ports[0].nodePort : "",
+      internal_dns: "couchdb-${var.service_id}.${var.service_namespace}",
+      external_ip: var.service_endpoint_type == "LoadBalancer" ? data.kubernetes_resources.couchdb_service.objects[0].status.loadBalancer.ingress[0].ip: ""
+    }
+  )
 }
